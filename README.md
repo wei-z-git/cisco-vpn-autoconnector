780a28c4f41fe42b51c8c682fa12f5e59bf94696
Wei Zhou <wei.zhou@partner.bmw.com>
2023-02-17 17:05:18 +0800

    1.fix path 2.add networksetup -setautoproxyurl "Wi-Fi"

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ Refer this repo for more information: https://gitee.com/amadeus666/mobile_push_k
 ## 2. Get your keys
 Examples in get-key-example.sh, getting keys from gitee
 ## 3. Run command
+cp -rf ../cisco-vpn-autoconnector ~/
 ./vpn-controller.sh [vpn_server] [qnumber] [pin]
 
 ## 4.(optional) Alias your Command
-cp -rf ../cisco-vpn-autoconnector ~/
 alias cvpn='~/cisco-vpn-autoconnector/vpn-controller.sh [vpn_server] [qnumber] [pin]'
--- vpn-controller.sh
+++ vpn-controller.sh
@@ -4,11 +4,12 @@ vpn-controller(){
     #1.connect 2.disconnected
     export vpn_status=`/opt/cisco/anyconnect/bin/vpn status |grep Disconnected`
     if [ -n "$vpn_status" ];then
-        source ./get-key-example.sh
+        networksetup -setautoproxyurl "Wi-Fi" "http://proxycn-sh.cn.bmwgroup.net/proxy.pac"
+        source ~/cisco-vpn-autoconnector/get-key-example.sh
         export vpn_server=$1
         export qnum=$2
         export pin=$3
-        expect ./connector
+        expect ~/cisco-vpn-autoconnector/connector
     else
         /opt/cisco/anyconnect/bin/vpn disconnect
     fi
