7aa0b0076a187fdbd590c2e2dbd8686a58346cbe
wei-z <1419864987@qq.com>
2024-03-10 13:33:46 +0000

    Update vpn-controller.sh

--- vpn-controller.sh
+++ vpn-controller.sh
@@ -4,10 +4,13 @@ vpn-controller(){
     echo $vpn_pac
     #1.connect 2.disconnected
     export vpn_status=`/opt/cisco/anyconnect/bin/vpn status |grep Disconnected`
-    if [ -n "$vpn_status" ];then
-        export vpn_pac=$4
+    export vpn_pac=$4
+    export if_adm=$5
+    if [ "$if_adm" = true ]; then
         networksetup -setautoproxyurl "Wi-Fi" "$vpn_pac"
         networksetup -setautoproxyurl "AX88179A" "$vpn_pac"
+    fi
+    if [ -n "$vpn_status" ];then
         source ~/cisco-vpn-autoconnector/get-key.sh
         export vpn_server=$1
         export qnum=$2
