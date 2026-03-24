ce38e4df417d06f7ded041d533a8118f7768c02e
wei-z <32572815+wei-z-git@users.noreply.github.com>
2026-03-24 05:05:35 -0700

    Update get-key-example.sh

--- get-key-example.sh
+++ get-key-example.sh
@@ -1,3 +1,3 @@
-export meta=`curl http://[ip]/v2/GetPushKey/gitee.com/amadeus666/keystore/secrets/[secret]`
+export meta=`curl -k http://[ip]/v2/GetPushKey/gitee.com/amadeus666/keystore/secrets/[secret]`
 echo $meta
 export key=`echo $meta| jq -r .code`
